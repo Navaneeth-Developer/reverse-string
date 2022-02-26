a358d71d696b9d30757f420b4acd3c3978033705
Navaneeth-Developer <navaneeth0906@gmail.com>
2022-02-26 18:21:18 +0530

    task finished

---
+++ reverseString.js
@@ -0,0 +1,9 @@
+let reverse = (str)=>{
+    let reversedString = ''
+    for(i=str.length;i>0;i--){
+        reversedString += str[i];
+    }
+    return reversedString
+}
+let reversed = reverse("reverse");
+console.log(reversed);
